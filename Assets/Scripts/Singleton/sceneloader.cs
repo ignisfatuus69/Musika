c848869ff8662965903da4747a3e771e5f24850a
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-04-01 01:09:43 +0800

    - Fixed unreferenced UI bug caused by importing DoTween

--- Assets/Scripts/Singleton/sceneloader.cs
+++ Assets/Scripts/Singleton/sceneloader.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
 public class sceneloader : MonoBehaviour
 {
+    public Image preImage;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,5 @@ public class sceneloader : MonoBehaviour
             SceneManager.UnloadSceneAsync(1);
             SceneManager.LoadScene(2, LoadSceneMode.Additive);
         }
-
     }
 }
