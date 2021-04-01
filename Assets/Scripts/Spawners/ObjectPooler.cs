94a2352cb7d4ff66742006741a59abd54d1c610b
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-04-01 16:58:37 +0800

    - Created Beat Class - Created Object Pooler Abstract Base Class - Creaded Beat Spawner (Inherited from Object Pooler) - Created Beat Base Prefab Object - Created Beat Spawner Prefab Object

---
+++ Assets/Scripts/Spawners/ObjectPooler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnObjectPooled : UnityEvent { };
+
+[System.Serializable]
+public class OnObjectSpawned : UnityEvent { };
+
+public abstract class ObjectPooler : MonoBehaviour
+{
+    public OnObjectSpawned EVT_OnObjectSpawned;
+    public OnObjectPooled EVT_OnObjectPooled;
+    public GameObject ObjectToSpawn;
+    public int SpawnCount = 1;
+
+    public List<GameObject> currentSpawnedObjects = new List<GameObject>();
+
+    public List<GameObject> pooledObjects = new List<GameObject>();
+    public Vector3 SpawnPosition;
+
+
+    public void Spawn()
+    {
+        for (int i = 0; i < SpawnCount; i++)
+        {
+
+            // Spawn the object. If we have an object in the pool, use that instead. Else, instantiate.
+            GameObject obj;
+            if (pooledObjects.Count > 0)
+            {
+                // get the last pooled object
+                obj = pooledObjects[0];
+                pooledObjects.RemoveAt(0);
+                obj.SetActive(true);
+                currentSpawnedObjects.Add(obj);
+
+            }
+            else
+            {
+                obj = Instantiate(ObjectToSpawn);
+                currentSpawnedObjects.Add(obj);
+                SetPoolingCondition(obj);
+            }
+
+            //Set Spawn Position
+            obj.transform.position = SpawnPosition;
+
+            EVT_OnObjectSpawned.Invoke();
+        }
+    }
+    protected abstract void SetPoolingCondition(GameObject obj);
+
+    protected virtual void Pool(GameObject obj)
+    {
+        obj.gameObject.SetActive(false);
+        pooledObjects.Add(obj.gameObject);
+        currentSpawnedObjects.Remove(obj.gameObject);
+    }
+
+
+
+}
