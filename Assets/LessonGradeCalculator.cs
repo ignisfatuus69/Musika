6e28153cf37df57eedae7834bdf8eea9ca10b599
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-04-12 17:02:01 +0800

    - Created lessongrade calculator for testing purposes - Created Life class (WIP)

---
+++ Assets/LessonGradeCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LessonGradeCalculator : MonoBehaviour
+{
+    [SerializeField] private SongData scriptableSongData;
+    [SerializeField] private Score scoreCounterObj;
+
+    private int perfectScore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CalculateLessonGrade();
+    }
+
+    private void CalculateLessonGrade()
+    {
+        CalculatePerfectScore();
+        Debug.Log("SS:" + perfectScore);
+        Debug.Log("S:" + perfectScore * .95f);
+        Debug.Log("A:" + perfectScore * .90f);
+        Debug.Log("B:" + perfectScore * .85f);
+        Debug.Log("C:" + perfectScore * .75f);
+        Debug.Log("D:" + perfectScore * .65f);
+    }
+
+    private void CalculatePerfectScore()
+    {
+        int noteCount = scriptableSongData.beatNoteIndexes.Count;
+        for (int i = 0; i < noteCount; i++)
+        {
+            perfectScore += (int)(scoreCounterObj.GetPerfectScoreValue + ((scoreCounterObj.GetPerfectScoreValue * (i) * 0.1) / 10));
+        }
+
+        for (int i = 0; i < noteCount; i++)
+        {
+            perfectScore += (int)(scoreCounterObj.GetPerfectScoreValue + ((scoreCounterObj.GetPerfectScoreValue * (i) * 0.1) / 10));
+        }
+    }
+}
--- Assets/Score.cs
+++ Assets/Score.cs
@@ -2,12 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SongGrade
+{
+    SS=0,
+    S=1,
+    A=2,
+    B=3,
+    C=4,
+    D=5,
+    F=6,
+
+}
+
 public class Score : Resource
 {
     [SerializeField] private BeatSpawner beatSpawnerObj;
     [SerializeField] private Combo comboCounterObj;
     [SerializeField] private int perfectScoreValue;
     [SerializeField] private int okayScoreValue;
+
+    public int GetPerfectScoreValue => perfectScoreValue;
+    public int GetOkayScoreValue => okayScoreValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +32,8 @@ public class Score : Resource
     void AddScore(Beat beatObj)
     {
         if (beatObj.beatState == BeatState.Miss) return;
-        if (beatObj.beatState == BeatState.Okay) this.currentValue += (int) (okayScoreValue +  (okayScoreValue * comboCounterObj.currentValue * 0.1) / 10);
-        else if (beatObj.beatState == BeatState.Perfect) this.currentValue += (int)(perfectScoreValue + (perfectScoreValue * comboCounterObj.currentValue * 0.1) / 10);
+        if (beatObj.beatState == BeatState.Okay) this.currentValue += (int) (okayScoreValue +  (okayScoreValue * ((comboCounterObj.currentValue-1) * 0.1) / 10));
+        else if (beatObj.beatState == BeatState.Perfect) this.currentValue += (int)(perfectScoreValue + ((perfectScoreValue * (comboCounterObj.currentValue-1) * 0.1) / 10));
 
         OnValueModified();
         EVT_OnValueAdded.Invoke();
