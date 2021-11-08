dfd3a96a338e4a7900f6d385e8db11d45df85d3b
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-11-08 19:16:01 +0800

    - Rescaled notebook -Setting up tutorial buttons on the notebook

---
+++ Assets/NotebookButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NotebookButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene(int sceneIndex)
+    {
+        SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+    }
+}
