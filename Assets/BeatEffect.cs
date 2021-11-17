997d2af2b14bbd2c91ffc8361b6daac71c2c9079
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-11-17 18:43:27 +0800

    - Added beat effects when beat is hit

---
+++ Assets/BeatEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatEffect : MonoBehaviour
+{
+    public OnDeactivate EVT_OnDeactivate;
+    [SerializeField] private float despawnTime;
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        StartCoroutine(DeactivateInSeconds());
+    }
+    IEnumerator DeactivateInSeconds()
+    {
+        yield return new WaitForSeconds(despawnTime);
+        EVT_OnDeactivate.Invoke(this.gameObject);
+    }
+}
---
+++ Assets/BeatEffectSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatEffectSpawner : ObjectPooler
+{
+    [SerializeField] private BeatSpawner beatSpawnerObj;
+
+
+    private void Start()
+    {
+        beatSpawnerObj.EVT_OnBeatPooled.AddListener(SpawnBeatEffect);
+    }
+   
+    protected override void SetPoolingInitializations(GameObject obj)
+    {
+        BeatEffect beatEffect = obj.GetComponent<BeatEffect>();
+        beatEffect.EVT_OnDeactivate.AddListener(Pool);
+    }
+
+    private void SpawnBeatEffect(Beat beatObj)
+    {
+        if (beatObj.beatState == BeatState.Miss) return;
+        CopyBeatPosition(beatObj);
+        Spawn();
+    }
+
+    private void CopyBeatPosition(Beat beatObj)
+    {
+        this.SpawnPosition = beatObj.transform.position;
+    }
+}
