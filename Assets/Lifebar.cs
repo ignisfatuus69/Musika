8f18da789f4cee4db27bbe577997b0da4927e999
Clifton Kyle Marasigan <cliftonkyle.marasigan@beniled.edu.ph>
2021-11-17 18:16:56 +0800

    - Set different reduction health values based on the difficulty of each song

--- Assets/Lifebar.cs
+++ Assets/Lifebar.cs
@@ -7,15 +7,26 @@ public class Lifebar : MonoBehaviour
 {
     [SerializeField] private Life lifeSystemObj;
     [SerializeField] private Image barImage;
+    [SerializeField] private List<Color> colors = new List<Color>();
     // Start is called before the first frame update
     void Start()
     {
-        lifeSystemObj.EVT_OnValueModified.AddListener(AdjustBar);    
+        lifeSystemObj.EVT_OnValueModified.AddListener(AdjustBar);
+        barImage.color = colors[0];
     }
 
     private void AdjustBar()
     {
+        float value = lifeSystemObj.currentValue / lifeSystemObj.GetMaximumValue;
+        barImage.DOFillAmount(value, 0.25f);
+        ChangeColor(value);
 
-        barImage.DOFillAmount(lifeSystemObj.currentValue / lifeSystemObj.GetMaximumValue, 0.25f);
+    }
+
+    private void ChangeColor(float value)
+    {
+        if (value > 0.70) barImage.color = colors[0];
+        if (value <= 0.70 && value >= 0.30) barImage.color = colors[1];
+        if (value < 0.30) barImage.color = colors[2];
     }
 }
