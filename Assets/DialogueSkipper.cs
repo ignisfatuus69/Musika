7aad1d729f9366dac34df4b844c05cae503b8e03
DESKTOP-GG9CD8Q\ignis <ignisalt3@gmail.com>
2022-06-07 20:48:10 +0800

    - Added dialogue skipper - Added object set active script

---
+++ Assets/DialogueSkipper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class DialogueSkipper : MonoBehaviour
+{
+    [SerializeField] private int[] timeSkipReferencesInSeconds;
+    [SerializeField] private PlayableDirector cutsceneDirector;
+    private int amountSkipped=0;
+
+    public void SkipToNextPart()
+    {
+        if (amountSkipped >= timeSkipReferencesInSeconds.Length) return;
+        StartCoroutine(Skip());
+      
+    }
+
+    IEnumerator Skip()
+    {
+        cutsceneDirector.Pause();
+        cutsceneDirector.time = timeSkipReferencesInSeconds[amountSkipped];
+        cutsceneDirector.Play();
+        yield return new WaitForSeconds(0.1f);
+        cutsceneDirector.Pause();
+        cutsceneDirector.Play();
+        amountSkipped += 1;
+    }
+}
---
+++ Assets/SetObjectActiveOnTime.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class SetObjectActiveOnTime : MonoBehaviour
+{
+    public bool isUsingDirector;
+    [SerializeField] private GameObject gameObjectToEnable;
+    [SerializeField] private PlayableDirector sceneDirector; 
+    [SerializeField] private int[] timesToEnable;
+    [SerializeField] private int[] timesToDisable;
+    private int enablingIndex = 0;
+    private int disablingIndex = 0;
+    private float timer=0;
+    private Coroutine SetObjectActiveCoroutine = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetObjectActiveCoroutine = StartCoroutine(SetObjectsAcive());
+        if(!isUsingDirector)
+        {
+            StartCoroutine(StartTimer());
+        }
+    }
+
+    IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(1);
+        timer += 1;
+    }
+
+    IEnumerator SetObjectsAcive()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (enablingIndex >= timesToEnable.Length)
+            {
+                gameObjectToEnable.SetActive(false);
+                StopCoroutine(SetObjectActiveCoroutine);
+            }
+
+
+            if (!isUsingDirector)
+            {
+                if (timer >= timesToEnable[enablingIndex])
+                {
+                    gameObjectToEnable.SetActive(true);
+                    enablingIndex += 1;
+                }
+                if (timer >= timesToDisable[disablingIndex])
+                {
+                    gameObjectToEnable.SetActive(false);
+                    disablingIndex += 1;
+                }
+                yield return null;
+            }
+
+            else if (isUsingDirector)
+            {
+                if (sceneDirector.time >= timesToEnable[enablingIndex])
+                {
+                    gameObjectToEnable.SetActive(true);
+                    enablingIndex += 1;
+                }
+                if (sceneDirector.time >= timesToDisable[disablingIndex])
+                {
+                    gameObjectToEnable.SetActive(false);
+                    disablingIndex += 1;
+                }
+                yield return null;
+            }
+        }
+    }
+}
